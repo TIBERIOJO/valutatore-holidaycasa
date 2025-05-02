1a93f35e487da2f84cece8f7a0eec34bd9516f9a
TIBERIOJO <JONATHAN@HOLIDAYCASA.COM>
2025-05-02 12:35:14 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,8 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ValutatoreSperlonga from './ValutatoreSperlonga';
-import './src/index.css'
-
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ValutatoreSperlonga />
