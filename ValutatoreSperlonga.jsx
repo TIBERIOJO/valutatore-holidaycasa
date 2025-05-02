76de738cf72fc8b6f7fd45c90d58cbde0ce6ba51
TIBERIOJO <JONATHAN@HOLIDAYCASA.COM>
2025-05-02 12:24:37 +0200

    Add files via upload

---
+++ ValutatoreSperlonga.jsx
@@ -0,0 +1,5 @@
+
+import { useState } from "react";
+export default function ValutatoreSperlonga() {
+  return <div>Valutatore Sperlonga - Inserisci i tuoi dati</div>;
+}
---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ValutatoreSperlonga from './src/ValutatoreSperlonga'
+import './src/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ValutatoreSperlonga />
+  </React.StrictMode>
+)
